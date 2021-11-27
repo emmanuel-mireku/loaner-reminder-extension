ee9b8c8d188a84f133ecf5407b8fd06b64efa81c
Emmanuel Mireku <34449042+emmanuel-mireku@users.noreply.github.com>
2021-11-27 10:47:51 -0500

    Add a few new features 🤣
    - Added the feature to check the type of platform. This will allow the extension to execute on only Chromebook devices.
    - Update the popup with "Time's up!" if the count down is up.
    - Added a function to ensure the background.js don't go to inactive. For some reason it goes inactive after a few minutes.

--- popup.js
+++ popup.js
@@ -1,54 +1,81 @@
-// let body = document.querySelector("#body");
-// let countdownTime = document.querySelector("#countdown-value");
-// let todaysDate = document.querySelector("#today-date");
-// let timeBorrowed = document.querySelector("#time-borrowed");
-// let returnMessage = document.querySelector("#return-message");
-// let actualTimeLeft = 0;
-
-
 let countdownTime = document.querySelector("#countdown-value");
 let todaysDate = document.querySelector("#today-date");
 let timeBorrowed = document.querySelector("#time-borrowed");
 let returnMessage = document.querySelector("#return-message");
 let timeToReturn = document.querySelector("#time-to-return");
-
-
-
-chrome.storage.local.get(null, ({ startDate, updatedTime, deadlineDate }) => {
-    timeBorrowed.innerHTML = startDate;
-    todaysDate.innerHTML = new Date().toLocaleDateString();
-    timeToReturn.innerHTML = deadlineDate;
-    if (updatedTime) {
-        let { days, hours, minutes, seconds } = updatedTime;
-        countdownTime.innerHTML = `${days}d: ${hours}h: ${minutes}m: ${seconds}s`;
-    } else {
-        let timeLeft = (new Date(deadlineDate).getTime() - new Date().getTime());
-        let days = Math.floor(timeLeft / 86400 / 1000);
-        let hours = Math.floor(timeLeft / 3600 / 1000);
-        let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (60 * 1000));
-        let seconds = Math.floor((timeLeft % (1000 * 60) / 1000));
-        countdownTime.innerHTML = `${days}d: ${hours}h: ${minutes}m: ${seconds}s`;
-    }
-});
-
-
-chrome.storage.onChanged.addListener((changes, area) => {
-    console.log(typeof changes);
-    console.log(changes, area);
-});
-
-
-chrome.runtime.sendMessage({ popupOpen: true });
-
-chrome.runtime.onMessage.addListener(function({ updatedTime, stopTime }, sender, sendResponse) {
-    if (!stopTime) {
-        let { days, hours, minutes, seconds } = updatedTime;
-        countdownTime.innerHTML = `${days}d: ${hours}h: ${minutes}m: ${seconds}s`;
-        console.log("background sent a fucking message bitch!", sender, sendResponse, updatedTime);
-    }
-});
+let popupBody = document.querySelector("#body");
 
 
 chrome.runtime.getPlatformInfo((platformInfo) => {
-    console.log(platformInfo);
+    let {
+        os
+    } = platformInfo;
+    if (os == "cros") {
+        console.log("This extension is intended to work for only Chromebooks ATM");
+        chrome.storage.sync.get(null, ({
+            startDate
+        }) => {
+            timeBorrowed.innerHTML = startDate;
+        });
+
+        chrome.storage.local.get(null, ({
+            updatedTime,
+            deadlineDate
+        }) => {
+            todaysDate.innerHTML = new Date().toLocaleDateString();
+            timeToReturn.innerHTML = deadlineDate;
+            if (updatedTime) {
+                let {
+                    days,
+                    hours,
+                    minutes,
+                    seconds
+                } = updatedTime;
+                countdownTime.innerHTML = `${days}d: ${hours}h: ${minutes}m: ${seconds}s`;
+                if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0) {
+                    countdownTime.innerHTML = "Time's Up!";
+                    returnMessage.innerHTML = "Please return Chromebook."
+                }
+            } else {
+                let timeLeft = (new Date(deadlineDate).getTime() - new Date().getTime());
+                let days = Math.floor(timeLeft / 86400 / 1000);
+                let hours = Math.floor(timeLeft / 3600 / 1000);
+                let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (60 * 1000));
+                let seconds = Math.floor((timeLeft % (1000 * 60) / 1000));
+                countdownTime.innerHTML = `${days}d: ${hours}h: ${minutes}m: ${seconds}s`;
+                if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0) {
+                    countdownTime.innerHTML = "Time's Up!";
+                    returnMessage.innerHTML = "Please return Chromebook."
+                }
+            }
+        });
+
+        chrome.runtime.onMessage.addListener(function ({
+            updatedTime,
+            stopTime
+        }, sender, sendResponse) {
+            if (stopTime) {
+                countdownTime.innerHTML = "Time's Up!";
+                returnMessage.innerHTML = "Please return Chromebook."
+            }
+            let {
+                days,
+                hours,
+                minutes,
+                seconds
+            } = updatedTime;
+            countdownTime.innerHTML = `${days}d: ${hours}h: ${minutes}m: ${seconds}s`;
+            console.log("background sent a fucking message bitch!", sender, sendResponse, updatedTime);
+        });
+    }
+    let notWorkingMessage = "This extension works only on school managed loaner chromebooks.";
+    popupBody.innerHTML = "";
+    let p = document.createElement("h2");
+    popupBody.appendChild(p);
+    p.innerHTML = notWorkingMessage;
+    popupBody.style.height = "100px";
+    popupBody.style.width = "100px";
+    p.style.fontFamily = "Roboto";
+    p.style.padding = "12px";
+    p.style.textAlign = "center";
 });
